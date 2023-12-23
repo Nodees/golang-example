b048bdb5c88547112a79f490549aa7863ec7299b
samuel.silva <samuel.silva@fpf.br>
2023-12-23 16:41:21 -0400

    fix go.mod and add preload of address foreign key

--- controllers/userController.go
+++ controllers/userController.go
@@ -13,6 +13,7 @@ func UserList(c *gin.Context) {
 	var users []models.User
 
 	connection.DB.Find(&users)
+	connection.DB.Preload("Address").Find(&users)
 
 	c.JSON(http.StatusOK, gin.H{
 		"results": users,
