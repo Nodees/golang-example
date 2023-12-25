fdf4472f59ee5594fb2ccf095dd9318e6a0e87d9
samuel.silva <samuel.silva@fpf.br>
2023-12-25 19:23:15 -0400

    concertando update do endereço

--- controllers/address_controller.go
+++ controllers/address_controller.go
@@ -41,16 +41,21 @@ func AddressRetrieve(c *fiber.Ctx) error {
 
 func AddressUpdate(c *fiber.Ctx) error {
 	var address models.Address
-
 	id := c.Params("id")
+
 	if err := connection.DB.First(&address, id).Error; err != nil {
 		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{"error": "Record not found"})
 	}
 
-	updateData := new(models.Address)
+	var updateData map[string]interface{}
+	if err := c.BodyParser(&updateData); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid request body"})
+	}
+
 	if err := connection.DB.Model(&address).Updates(updateData).Error; err != nil {
 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Failed to update record"})
 	}
+
 	return c.JSON(address)
 }
 
