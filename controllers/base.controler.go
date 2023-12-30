d6398a4c8c568785be32b87013e5c3230bf19025
samuel.silva <samuel.silva@fpf.br>
2023-12-30 15:06:55 -0400

    Adicionado resultado paginado se ele tiver algum parametro que indica que ele vai precisar paginar

--- controllers/base.controler.go
+++ controllers/base.controler.go
@@ -9,10 +9,24 @@ import (
 	"gorm.io/gorm"
 )
 
+type PaginationParams struct {
+	Page     int `form:"page"`
+	PageSize int `form:"pageSize"`
+}
+
 func BaseList[T any](preloads ...string) fiber.Handler {
 	return func(c *fiber.Ctx) error {
 		models := new([]T)
 		query := connection.DB
+		params := new(PaginationParams)
+
+		if err := c.QueryParser(params); err != nil {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid query parameters"})
+		}
+
+		if params.Page != 0 || params.PageSize != 0 {
+			return BasePaginatedList[T](preloads...)(c)
+		}
 
 		if len(preloads) > 0 {
 			for _, preload := range preloads {
@@ -26,6 +40,65 @@ func BaseList[T any](preloads ...string) fiber.Handler {
 		}
 		c.SendStatus(http.StatusOK)
 		return c.JSON(models)
+	}
+}
+
+func BasePaginatedList[T any](preloads ...string) fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		models := new([]T)
+		query := connection.DB
+		params := new(PaginationParams)
+		var totalCount int64
+
+		if err := c.QueryParser(params); err != nil {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid query parameters"})
+		}
+
+		if params.Page <= 0 {
+			params.Page = 1
+		}
+
+		defaultPageSize := 10
+		if params.PageSize <= 0 {
+			params.PageSize = defaultPageSize
+		}
+
+		if len(preloads) > 0 {
+			for _, preload := range preloads {
+				query = query.Preload(preload)
+			}
+		}
+
+		if err := query.Model(models).Count(&totalCount).Error; err != nil {
+			return err
+		}
+
+		totalPages := int(totalCount / int64(params.PageSize))
+		if totalCount%int64(params.PageSize) > 0 {
+			totalPages++
+		}
+
+		if params.Page == totalPages && totalCount%int64(params.PageSize) == 0 {
+			totalPages--
+		}
+
+		offset := (params.Page - 1) * params.PageSize
+		query = query.Offset(offset).Limit(params.PageSize)
+
+		result := query.Find(models)
+		if result.Error != nil {
+			return result.Error
+		}
+
+		response := fiber.Map{
+			"total_pages":  totalPages,
+			"current_page": params.Page,
+			"page_size":    params.PageSize,
+			"result":       models,
+		}
+
+		c.Status(fiber.StatusOK)
+		return c.JSON(response)
 	}
 }
 
